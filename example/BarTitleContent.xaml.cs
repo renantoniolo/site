cd4bf9b311a5044fa174375056d03568500b6ad4
Renan Toniolo Rocha <renantoniolo@gmail.com>
2019-05-05 23:32:42 -0300

    Exemplo codigo

---
+++ example/BarTitleContent.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace zeroAcidentesApp.Component
+{
+    public partial class BarTitleContent : ContentView
+    {
+        public BarTitleContent()
+        {
+            InitializeComponent();
+        }
+
+        public string TitleText
+        {
+            get { return base.GetValue(TitleTextProperty).ToString(); }
+            set { base.SetValue(TitleTextProperty, value); }
+        }
+
+        public static readonly BindableProperty TitleTextProperty = BindableProperty.Create(
+            propertyName: "TitleText",
+            returnType: typeof(string),
+            declaringType: typeof(BarTitleContent),
+            defaultValue: "",
+            defaultBindingMode: BindingMode.TwoWay,
+            propertyChanged: TitleTextPropertyChanged);
+
+        private static void TitleTextPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (BarTitleContent)bindable;
+            control.xTitulo.Text = newValue.ToString();
+        }
+
+    }
+}
